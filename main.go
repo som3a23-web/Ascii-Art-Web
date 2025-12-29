faeb139bdf6a3d75e4cb7daffd55ba97bc58f956
ansmoner <ansmoner2000@gmail.com>
2025-12-29 11:23:31 +0200

    Refactor PageData structure and remove unused variables in homeHandler function

--- main.go
+++ main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 )
 
 type PageData struct {
-	Title  string
-	Body   string
 	Input  string
 	Banner string
 	Art    string
@@ -18,15 +15,12 @@ type PageData struct {
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	Input := r.FormValue("textInput")
 	Banner := r.FormValue("banner")
-
 	tmpl, err := template.ParseFiles("./template/index.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	err = tmpl.Execute(w, PageData{Input: Input, Banner: Banner})
-	fmt.Println(Input)
-	fmt.Print(Banner)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
@@ -37,12 +31,6 @@ func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/", homeHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
-
-	// stringToArt, banner := ascii.StoreInputAndBanner()
-	// bannerFile := ascii.ReadBanner(banner)
-	// bannerSlice := strings.Split(bannerFile, "\n")
-	// splitInput := strings.Split(stringToArt, "\\n")
-	// outPut := ascii.DrawingInput(splitInput, bannerSlice)
 
 }
 
